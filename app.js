4e46efa436f4cd31a98ad7acc4d69495152989c7
Marton <bfmarton@gmail.com>
2018-10-19 10:07:09 +0200

    Add books.ejs Create books route GET

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("The landing page");
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("landing");
+});
+
+app.get("/books", (req, res) => {
+    let books = [
+        {
+            title: "Harry Potter",
+            image: "https://images.unsplash.com/photo-1530027801118-fee21a67b3af?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5e271bf72dbc65bf5970a850e314d5ce&auto=format&fit=crop&w=750&q=80"
+        },
+        {
+            title:"Guiness World Records",
+            image: "https://images.unsplash.com/photo-1527422265102                     -22027ee90fcd?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a9a1bba5014e41cb84a45574a9c87901&auto=format&fit=crop&w=700&q=60"
+        }
+    ];1
+    res.render("books");
 });
 
 app.listen(3000, () =>
