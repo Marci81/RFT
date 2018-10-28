81cd58cfbe85d20bc7c36410372251d41533cae6
Marton <bfmarton@gmail.com>
2018-10-28 11:56:08 +0100

    Finish Navbar

--- app.js
+++ app.js
@@ -8,26 +8,38 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname + '/public'));
 
+//TODO Delete After DB Added
+let books = [
+    {
+        title: "Harry Potter",
+        image: "https://images.unsplash.com/photo-1530027801118-fee21a67b3af?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5e271bf72dbc65bf5970a850e314d5ce&auto=format&fit=crop&w=750&q=80"
+    },
+    {
+        title:"Guiness World Records",
+        image: "https://images.unsplash.com/photo-1532348374062-fee19177e98f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d4086d5d36662ba037e49111340611aa&auto=format&fit=crop&w=700&q=60"
+    }
+];
+
 app.get("/", (req, res) => {
     res.render("landing");
 });
 
 app.get("/books", (req, res) => {
-    let books = [
-        {
-            title: "Harry Potter",
-            image: "https://images.unsplash.com/photo-1530027801118-fee21a67b3af?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5e271bf72dbc65bf5970a850e314d5ce&auto=format&fit=crop&w=750&q=80"
-        },
-        {
-            title:"Guiness World Records",
-            image: "https://images.unsplash.com/photo-1532348374062-fee19177e98f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d4086d5d36662ba037e49111340611aa&auto=format&fit=crop&w=700&q=60"
-        }
-    ];
+
     res.render("books",{books:books});
 });
 
 app.post("/books",(req,res)=>{
-    res.send("You hit the post route");
+
+    let title = req.body.title;
+    let image = req.body.image;
+    let newBook ={
+        title:title,
+        image:image
+    };
+    books.push(newBook);
+
+    res.redirect("/books");
 });
 
 app.get("/books/new",(req,res)=>{
