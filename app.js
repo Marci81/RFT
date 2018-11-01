7b77b9c17c27c637d66353d6383cc5222bfe0d1a
Marton <bfmarton@gmail.com>
2018-11-01 11:53:22 +0100

    Add show.ejs

--- app.js
+++ app.js
@@ -3,47 +3,39 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/bookReviewRft", {useNewUrlParser: true});
+
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 
-//TODO Delete After DB Added
-let books = [
-    {
-        title: "Harry Potter",
-        image: "https://images.unsplash.com/photo-1530027801118-fee21a67b3af?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5e271bf72dbc65bf5970a850e314d5ce&auto=format&fit=crop&w=750&q=80"
-    },
-    {
-        title:"Guiness World Records",
-        image: "https://images.unsplash.com/photo-1532348374062-fee19177e98f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d4086d5d36662ba037e49111340611aa&auto=format&fit=crop&w=700&q=60"
-    },
-    {
-        title: "Harry Potter",
-        image: "https://images.unsplash.com/photo-1530027801118-fee21a67b3af?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5e271bf72dbc65bf5970a850e314d5ce&auto=format&fit=crop&w=750&q=80"
-    },
-    {
-        title:"Guiness World Records",
-        image: "https://images.unsplash.com/photo-1532348374062-fee19177e98f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d4086d5d36662ba037e49111340611aa&auto=format&fit=crop&w=700&q=60"
-    },
-    {
-        title: "Harry Potter",
-        image: "https://images.unsplash.com/photo-1530027801118-fee21a67b3af?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5e271bf72dbc65bf5970a850e314d5ce&auto=format&fit=crop&w=750&q=80"
-    },
-    {
-        title:"Guiness World Records",
-        image: "https://images.unsplash.com/photo-1532348374062-fee19177e98f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d4086d5d36662ba037e49111340611aa&auto=format&fit=crop&w=700&q=60"
-    },
-    {
-        title: "Harry Potter",
-        image: "https://images.unsplash.com/photo-1530027801118-fee21a67b3af?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5e271bf72dbc65bf5970a850e314d5ce&auto=format&fit=crop&w=750&q=80"
-    },
-    {
-        title:"Guiness World Records",
-        image: "https://images.unsplash.com/photo-1532348374062-fee19177e98f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d4086d5d36662ba037e49111340611aa&auto=format&fit=crop&w=700&q=60"
+
+//TODO Separate them later
+let bookSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    description: String
+});
+
+let Book = mongoose.model("Book", bookSchema);
+
+
+//TODO Create New Book Just Uncomment The Following Code
+Book.create({
+    title: "Harry Potter",
+    image: "https://images.unsplash.com/photo-1530027801118-fee21a67b3af?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5e271bf72dbc65bf5970a850e314d5ce&auto=format&fit=crop&w=750&q=80",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae varius ipsum, non ullamcorper ex. Morbi aliquam consectetur ligula, nec fringilla metus tempus vitae. Nulla facilisi. Etiam quis convallis tortor. Integer auctor efficitur dapibus."
+}, function (err, book) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("New Book:");
+        console.log(book);
     }
-];
+});
+
 
 app.get("/", (req, res) => {
     res.render("landing");
@@ -51,30 +43,41 @@ app.get("/", (req, res) => {
 
 app.get("/books", (req, res) => {
 
-    res.render("books",{books:books});
+    Book.find({}, function (err, booksFromDB) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("books", {books: booksFromDB});
+        }
+    });
+
 });
 
-app.post("/books",(req,res)=>{
+app.post("/books", (req, res) => {
 
     let title = req.body.title;
     let image = req.body.image;
-    let newBook ={
-        title:title,
-        image:image
+    let newBook = {
+        title: title,
+        image: image
     };
-    books.push(newBook);
 
-    res.redirect("/books");
+    Book.create(newBook, (err, newBookToDB) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/books");
+        }
+    });
 });
 
-app.get("/books/new",(req,res)=>{
+app.get("/books/new", (req, res) => {
     res.render("new.ejs")
 });
 
-app.get("/books/:id",function (req, res) {
-   res.render("partials/header") ;
+app.get("/books/:id", (req, res) => {
+    res.render("show");
 });
-
 
 
 app.listen(3000, () =>
